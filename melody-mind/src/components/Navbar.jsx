3600c1349a8b2bd89e59f048e7ca8625837f52ca
Kyle-Anthony Hay <haykyle917@gmail.com>
2024-02-14 17:10:54 -0500

    Aceternity Added

--- melody-mind/src/components/Navbar.jsx
+++ melody-mind/src/components/Navbar.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import './styles/Navbar.css'
+import { Meteors } from "./ui/meteors"; 
 
 const Navbar = () => {
   return (
-    <nav>
-      <div class="logo">Melody Mind</div>
-      <div>
-        <ul class="nav-links">
-          <li><a href="#projects">Sign Up</a></li>
-          <li><a href="#contact">Log In</a></li>
-        </ul>
-      </div>
-    </nav>
+    <>
+      <nav>
+        <div class="logo">Melody Mind</div>
+        <div>
+          <ul class="nav-links">
+            <li><a href="#projects">Sign Up</a></li>
+            <li><a href="#contact">Log In</a></li>
+          </ul>
+        </div>
+      </nav>
+      
+    </>
   )
 }
 
